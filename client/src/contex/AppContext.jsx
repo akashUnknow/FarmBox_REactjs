9a100589c89bbe711f1b11849bb8c214303f8027
akashUnknow <akash.unknow@gmail.com>
2025-04-19 11:06:22 +0000

    2h

--- client/src/contex/AppContext.jsx
+++ client/src/contex/AppContext.jsx
@@ -1,16 +1,67 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext=createContext();
 
 export const AppContextProvider=({children})=>{
+    const currency=import.meta.VITE_CURRENCY
     const navigate=useNavigate();
     const [user,SetUser]=useState(null);
     const [isSeller,SetIsSeller]=useState(false);
     const [showUserLogin,SetshowUserLogin]=useState(false);
-    // const [user,SetUser]=useState(null);
+    const [products,setProducts]=useState([]);
+    const [cartItems,setCartItems]=useState({});
+    // fetch all product
+    const fetchProducts=async ()=>{
+        setProducts(dummyProducts)
+    }
+
+    // add product to cart
+    const addToCart= async (itemId)=>{
+        let cartData=structuredClone(cartItems)
+        if(cartData[itemId]){
+            cartData[itemId]+=1
+        }else{
+            cartData[itemId]=1
+        }
+        setCartItems(cartData)
+        toast.success("Added to cart")
+    }
+
+    // update card iteam quantity
+    const updateCartItems=(itemId,quantity)=>{
+        let cartData=structuredClone(cartItems)
+        cartData[itemId]=quantity;
+        setCartItems(cartData)
+        toast.success("cart updated")
+    }
+
+    //remove from cart
+
+    const removeFromCart=(itemId)=>{
+        let cartData=structuredClone(cartItems)
+        if(cartData[itemId]){
+            cartData[itemId]-=1
+            if(cartData[itemId]===0){
+                delete cartData[itemId]
+
+            }
+        }
+        setCartItems(cartData)
+        toast.success("Remove from cart")
+    }
+
+    useEffect(()=>{
+        fetchProducts();
+        console.log(products)
+    },[])
+
     
-    const value ={navigate,user,SetUser,isSeller,SetIsSeller,showUserLogin ,SetshowUserLogin}
+    const value ={navigate,user,SetUser,isSeller,SetIsSeller,showUserLogin ,
+        SetshowUserLogin,currency,addToCart,updateCartItems,products,
+        removeFromCart,cartItems}
 
     return <AppContext.Provider value={value}>
         {children}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+import BestSeller from '../components/BestSeller'
+import BottamBanner from '../components/BottamBanner'
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <MainBanner/>
+      <Categories/>
+      <BestSeller/>
+      <BottamBanner/>
     </div>
   )
 }
